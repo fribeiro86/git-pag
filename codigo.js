16a64c6d67bb8b616018e1076d486e5793008c0e
Fabricio Ribeiro <76857411+fribeiro86@users.noreply.github.com>
2021-07-01 15:52:06 -0300

    Atualizado

---
+++ codigo.js
@@ -0,0 +1,20 @@
+  var x = "red";
+
+  function atual() {
+
+    if (x === "red") {
+      document.getElementById("current").style.backgroundColor = x;
+      document.getElementById("current").style.color = "white";
+      x = "white"; 
+    } else {
+      document.getElementById("current").style.backgroundColor = x;
+      document.getElementById("current").style.color = "black";
+      x= "red";
+    }
+
+    var id = setTimeout(() => {
+      atual();
+    }, 600);
+  }
+
+  window.addEventListener("load",atual); 
